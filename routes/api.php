8d1d78397303ee0a82d134c305b3c7533f30b8c4
Maciej Zolnierczyk <maciej.zolnierczyk@studenci.collegiumwitelona.pl>
2024-03-12 11:07:48 +0100

    Added basic api routes for user

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CardsController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,13 +14,20 @@ use App\Http\Controllers\CardsController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::middleware(['auth:sanctum'])->group(function () {
 
+    //User routes
+    Route::post('/register', [UserController::class, 'RegisterUser']);
+    Route::post('/login', [UserController::class, 'LoginUser']);
+    Route::post('/logout', [UserController::class, 'LogoutUser']);
+    Route::post('/whoami', [UserController::class, 'GetCurrentUser']);
 
+    //Cards routes
+    Route::get('/cards', [CardsController::class, 'index']);
+    Route::post('/cards', [CardsController::class, 'store']);
+    Route::get('/cards/{id}', [CardsController::class, 'show']);
+    Route::put('/cards/{id}', [CardsController::class, 'update']);
+    Route::delete('/cards/{id}', [CardsController::class, 'destroy']);
+    Route::get("data",[CardsController::class, "getData"]);
+});
 
-Route::get('/cards', [CardsController::class, 'index']);
-Route::post('/cards', [CardsController::class, 'store']);
-Route::get('/cards/{id}', [CardsController::class, 'show']);
-Route::put('/cards/{id}', [CardsController::class, 'update']);
-Route::delete('/cards/{id}', [CardsController::class, 'destroy']);
-
-Route::get("data",[CardsController::class, "getData"]);
