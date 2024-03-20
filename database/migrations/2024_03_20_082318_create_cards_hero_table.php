ce20b5e8359b9dd8d369b6f6e91f8df6f8655d7f
Maciej Zolnierczyk <maciej.zolnierczyk@studenci.collegiumwitelona.pl>
2024-03-20 09:25:36 +0100

    added cards hero migration

---
+++ database/migrations/2024_03_20_082318_create_cards_hero_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cards_hero', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('talentDescription');
+            $table->integer('talentCooldown');
+            $table->string('talentType');
+            $table->integer('talentValue');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cards_hero');
+    }
+};
