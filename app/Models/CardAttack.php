0e2218cc3acdc17688be3e992f4a0918914d97cf
Maciej Zolnierczyk <maciej.zolnierczyk@studenci.collegiumwitelona.pl>
2024-03-20 09:00:27 +0100

    created models for both type of cards

---
+++ app/Models/CardAttack.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CardAttack extends Model
+{
+    use HasFactory;
+    protected $table = 'cardsAttack';
+    protected  $fillable = [
+        'element',
+        'energyCost',
+        'cardName',
+        'cardImage',
+        'cardBasicDescription',
+        'cardGoldDescription',
+        'AttackLeft',
+        'AttackFront',
+        'AttackRight'
+    ];
+}
---
+++ app/Models/CardDefend.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CardDefend extends Model
+{
+    use HasFactory;
+    protected $table = 'cardsDefend';
+    protected  $fillable = [
+        'element',
+        'energyCost',
+        'cardName',
+        'cardImage',
+        'cardBasicDescription',
+        'cardGoldDescription',
+        'AttackLeft',
+        'AttackFront',
+        'AttackRight'
+    ];
+}
