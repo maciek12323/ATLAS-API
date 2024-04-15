6a57b354395dd3684d9cd7e1ea0bf1179691299c
Maciej Zolnierczyk <maciej.zolnierczyk@studenci.collegiumwitelona.pl>
2024-04-15 09:31:11 +0200

    Defined migration for statistics

--- database/migrations/2024_04_15_071828_create_statistics_table.php
+++ database/migrations/2024_04_15_071828_create_statistics_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('statistics', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->integer('games_played');
+            $table->integer('games_won');
+            $table->integer('games_lost');
             $table->timestamps();
         });
     }
