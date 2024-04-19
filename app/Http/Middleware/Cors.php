4c44065e0ca960852752ac35f0507d17b6fe5ead
Maciej Zolnierczyk <maciej.zolnierczyk@studenci.collegiumwitelona.pl>
2024-04-19 09:29:52 +0200

    Added cors class to kernel.php and rewrited routes

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -12,7 +12,7 @@ class Cors
     {
         $response = $next($request);
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         return $response;
